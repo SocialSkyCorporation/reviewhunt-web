f5b2f69966a2fc649f7fa8b1850eedc540cfa036
ggomaeng <sungwoopark95@gmail.com>
2019-07-08 17:04:52 +0900

    Update SubmittedItem.js

--- src/pages/Profile/MakerProfile/SubmittedItem.js
+++ src/pages/Profile/MakerProfile/SubmittedItem.js
@@ -1,20 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SimpleButton from "components/SimpleButton";
+import approvedImg from 'assets/images/approved.svg';
+import rejectedImg from 'assets/images/rejected.svg';
 
-const SubmittedItem = ({}) => {
+const SubmittedItem = ({noBorder, onClick, approved, rejected}) => {
   return (
-    <div className="submitted-item">
+    <div className="submitted-item" onClick={onClick}>
       <img className="submitted-item-img" />
-      <div className="submitted-item-type text-small text-grey uppercase">Quest 1</div>
-      <div className="submitted-item-title text-black">Make your first bark.</div>
+      <div className="submitted-item-type text-small text-grey uppercase">
+        Quest 1
+      </div>
+      <div className="submitted-item-title text-black">
+        Make your first bark.
+      </div>
       <div className="text-small text-grey">YoungHwi Cho</div>
       <div className="text-small text-grey">5 min ago</div>
-      <div className="row-space-between" style={{marginTop: 16}}>
-        <SimpleButton type="danger" className="submitted-item-button" text="Reject" />
+      <div className="row-space-between" style={{ marginTop: 16 }}>
+        <SimpleButton
+          type="danger"
+          className="submitted-item-button"
+          text="Reject"
+        />
         <SimpleButton className="submitted-item-button" text="Approve" />
       </div>
-      <div className="bottom-divider"/>
+      {!noBorder && <div className="bottom-divider" />}
+      {approved && <img className="approved-rejected-icon" src={approvedImg} alt=""/>}
+      {rejected && <img className="approved-rejected-icon" src={rejectedImg} alt=""/>}
     </div>
   );
 };
@@ -22,5 +34,7 @@ const SubmittedItem = ({}) => {
 SubmittedItem.propTypes = {};
 
 SubmittedItem.defaultProps = {};
+
+export const EmptySubmittedItem = () => <div className="submitted-item" />;
 
 export default SubmittedItem;
