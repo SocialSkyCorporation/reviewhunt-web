5a2c1b5cca9abecdbd7d33fa4f1c7856aa0a9d57
ggomaeng <sungwoopark95@gmail.com>
2019-07-10 17:43:01 +0900

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,43 +2,52 @@ import React from "react";
 import { AuthConsumer } from "contexts/AuthContext";
 import { Link } from "react-router-dom";
 import imgLogo from "assets/images/logo-rh@2x.png";
+import imgLogoTeal from "assets/images/logo-rh.svg";
 import faceImg from "assets/images/user-face.svg";
 import { useTranslation } from "react-i18next";
 
 export default () => {
   const { t } = useTranslation();
+  const pathname = window.location.pathname;
+  const isAboutPage = pathname.indexOf("/about") > -1;
+
+  const headerItemClassName = `header-item ${isAboutPage && 'about'}`;
 
   return (
     <AuthConsumer>
       {({ emailMe }) => (
         <div className="header">
           <Link to="/">
-            <img className="logo" src={imgLogo} alt="logo" />
+            <img
+              className="logo"
+              src={isAboutPage ? imgLogoTeal : imgLogo}
+              alt="logo"
+            />
           </Link>
 
           <div className="header-right">
-            <Link className="header-item" to="/about">
+            <Link className={headerItemClassName} to="/about">
               {t("header.about")}
             </Link>
 
             {!emailMe ? (
               <>
-                <Link className="header-item" to="/auth">
+                <Link className={headerItemClassName}  to="/auth">
                   {t("header.login")}
                 </Link>
 
-                <Link className="header-item" to="/auth">
+                <Link className={headerItemClassName}  to="/auth">
                   {t("header.join")}
                 </Link>
 
-                <Link className="header-item" to="/profile">
+                <Link className={headerItemClassName} to="/profile">
                   {t("header.profile")}
                 </Link>
               </>
             ) : (
-              <Link className="header-item account" to="/profile">
+              <Link className={`header-item account ${isAboutPage && 'about'}`} to="/profile">
                 <div className="row-align-center">
-                  <img className="profile-icon" src={faceImg} alt=""/>
+                  <img className="profile-icon" src={faceImg} alt="" />
                   <div>{emailMe.name}</div>
                 </div>
               </Link>
