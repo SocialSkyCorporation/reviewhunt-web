2303e29cb064b81d41aa31a548817aa94b1395ec
ggomaeng <sungwoopark95@gmail.com>
2019-07-11 14:50:11 +0900

    Update FullWidthButton to take icon prop

--- src/components/FullWidthButton.js
+++ src/components/FullWidthButton.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const FullWidthButton = ({ text, style, onClick }) => {
+const FullWidthButton = ({ text, style, onClick, icon }) => {
   return (
     <div className="full-width-button" style={style} onClick={onClick}>
+    {icon && <div className="icon">
+      {icon}
+    </div>}
       {text}
     </div>
   );
@@ -13,6 +16,10 @@ FullWidthButton.propTypes = {
   text: PropTypes.string,
   style: PropTypes.object,
   onClick: PropTypes.func
+};
+
+FullWidthButton.defaultProps = {
+  icon: null
 };
 
 export default FullWidthButton;
