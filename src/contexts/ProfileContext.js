90fa38cb6c3697be88766f3cebc6b29c01ef5b51
ggomaeng <sungwoopark95@gmail.com>
2019-07-22 11:32:43 +0900

    Listen to location change by backpress

--- src/contexts/ProfileContext.js
+++ src/contexts/ProfileContext.js
@@ -6,24 +6,35 @@ import {
   updateLocation
 } from "utils/helpers/urlHelper";
 import queryString from "query-string";
-import _ from "lodash";
+import { withRouter } from "react-router-dom";
 import { getToken } from "utils/token";
 import { TYPE_HUNTER, TYPE_MAKER } from "pages/Auth";
 import { TAB_PROFILE } from "pages/Profile/HunterProfile";
 import { TAB_CREATE_CAMPAIGN } from "pages/Profile/MakerProfile";
+import _ from "lodash";
 
 const ProfileContext = React.createContext();
 const { Provider, Consumer } = ProfileContext;
 
 class ProfileProvider extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       tabIndex: null,
       campaignId: null,
       editProfile: false
     };
+    this.unlisten = props.history.listen(async (location, action) => {
+      const query = getParams(location);
+      if (!_.isEmpty(query) && getRouteName(window.location) === "profile") {
+        await this.setState({ ...query });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unlisten();
   }
 
   async componentDidMount() {
@@ -65,6 +76,11 @@ class ProfileProvider extends React.Component {
   }
 }
 
-export { ProfileProvider, Consumer as ProfileConsumer };
+const ProfileProviderWithRouter = withRouter(ProfileProvider);
+
+export {
+  ProfileProviderWithRouter as ProfileProvider,
+  Consumer as ProfileConsumer
+};
 
 export default ProfileContext;
